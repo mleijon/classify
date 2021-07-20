51dd65714285153d0fdbbfa51f9780eee107e5a5
mleijon <mikael.leijon@gmail.com>
2021-07-21 00:15:15 +0200

    classify v3

--- taxid2seqs.py
+++ taxid2seqs.py
@@ -7,32 +7,31 @@ from collections import defaultdict
 
 
 class TxidHits:
-    merged_results = list(dict(dict()))
     seq_set = set()
 
     def __init__(self, sa, txid):
-        def make_reduced_daa(ddaf):
-            red_daa = defaultdict(dict)
+        self.merged_results = defaultdict(dict)
+
+        def make_reduced_seqs(ddaf):
+            red_seqs = defaultdict(dict)
             seqname_set = set()
-            red_seqs = dict()
             with gzip.open(ddaf, 'rt') as a:
                 for line in a:
                     if line.split()[1] == txid:
-                        red_daa[line.split(';')[0]]['size'] = \
+                        red_seqs[line.split(';')[0]]['size'] = \
                             line.split(';')[1][5:]
-                        red_daa[line.split(';')[0]]['evalue'] = \
+                        red_seqs[line.split(';')[0]]['evalue'] = \
                             line.split()[2].strip()
                         seqname_set.add(line.split(';')[0])
             with gzip.open(ddaf.replace('.daa', '_uq.fa'), 'rt') as f:
-                x = 0
                 while True:
                     line = f.readline()
                     if not line:
                         break
                     if line.split(';')[0][1:] in seqname_set:
-                        x= x + int(red_daa[line.split(';')[0][1:]]['size'])
-                        red_daa[line.split(';')[0][1:]]['seq'] = next(f)
-            print(x)
+                        red_seqs[next(f).strip()] = \
+                            red_seqs.pop(line.split(';')[0][1:])
+            return red_seqs
         self.sample = sa
         self.txid = id
         dirs = []
@@ -40,10 +39,16 @@ class TxidHits:
         for split in os.listdir(self.home):
             dirs.append(os.path.join(self.home, split))
         for subdir in dirs:
-            print(subdir)
             for file in os.listdir(subdir):
                 if file.endswith(".daa.gz"):
-                    make_reduced_daa(os.path.join(subdir, file))
+                    split = make_reduced_seqs(os.path.join(subdir, file))
+                    for key in split:
+                        if key not in self.merged_results.keys():
+                            self.merged_results.update({key:split[key]})
+                        else:
+                            new_size = str(int(self.merged_results[key]['size']
+                                               ) + int(split[key]['size']))
+                            self.merged_results[key]['size'] = new_size
 
 
 if __name__ == "__main__":
@@ -89,3 +94,7 @@ if __name__ == "__main__":
                     os.remove(os.path.join(parent_dir, file))
 
     test = TxidHits(samples[0], ARGS.inp[1])
+    x = 0
+    for key in test.merged_results:
+        x += int(test.merged_results[key]['size'])
+    print(x)
